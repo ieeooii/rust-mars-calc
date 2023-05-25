1851cf85ba2851c69276b07e47f8cdea0451da2f
CLOwinnie <winnie@clo3d.com>
2023-05-25 22:53:35 +0900

    macro - fn return value into println

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 fn main() {
-    println!("Number: {}, String: {}", 100, "abc");
-    calculate_weight_on_mars(100.0);
+    println!("Weight on Mars: {}kg", calculate_weight_on_mars(100.0));
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
-    50.0
+    (weight / 9.81) * 3.711
 }
