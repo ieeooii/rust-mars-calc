d01ae6d65b8a59f7d42b7fa15a9ae8d582fb1003
CLOwinnie <winnie@clo3d.com>
2023-05-25 22:40:09 +0900

    printIn! test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    println!("Hello, world!");
-    calculate_weight_on_mars(100.0)
+    println!("Number: {}, String: {}", 100, "abc");
+    calculate_weight_on_mars(100.0);
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
     50.0
-} 
+}
