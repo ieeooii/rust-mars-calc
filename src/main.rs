c151da8bc94cc498777283b20fbd93a173552823
CLOwinnie <winnie@clo3d.com>
2023-06-02 00:24:43 +0900

    lldb debug

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,23 @@ use std::io;
 
 fn main() {
     let mut input = String::new();
+
     io::stdin().read_line(&mut input);
-    let mars_weight = calculate_weight_on_mars(100.0);
-    println!("Weight on Mars: {}kg", mars_weight);
+
+    borrow_string(&input);
+    own_string(input);
+
+    // println!("Input: {}", input);
+    // let mars_weight = calculate_weight_on_mars(100.0);
+    // println!("Weight on Mars: {}kg", mars_weight);
+}
+
+fn borrow_string(s: &String) {
+    print!("{}", s);
+}
+
+fn own_string(s: String) {
+    print!("{}", s);
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
